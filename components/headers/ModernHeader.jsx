8d162ad32fd82c76fbca8c2f9324cc0d935ef1f5
99-Malik <usamaiftikhar718@gmail.com>
2025-09-26 10:06:01 +0300

    new changes

--- components/headers/ModernHeader.jsx
+++ components/headers/ModernHeader.jsx
@@ -127,7 +127,7 @@ const ModernHeader = () => {
                     <div className="w-10 h-10 rounded-lg bg-gradient-to-br from-blue-600 to-purple-600 flex items-center justify-center">
                       <Wrench className="w-6 h-6 text-white" />
                     </div>
-                    <span className="text-xl font-bold text-gray-900">
+                    <span className="text-md font-bold text-gray-900">
                       Appliance Repairs Pro
                     </span>
                   </div>
