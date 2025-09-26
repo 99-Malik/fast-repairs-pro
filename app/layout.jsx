6c25d91a5385e44575a72b52f37d932164025c84
99-Malik <usamaiftikhar718@gmail.com>
2025-09-26 10:08:43 +0300

    new changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -17,52 +17,20 @@ export default function RootLayout({ children }) {
     <html lang="en" className={inter.className}>
     <head>
         {/* Include gtag.js script */}
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-16624322208" />
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-17521374535" />
 
         {/* Include gtag.js initialization script */}
         <Script dangerouslySetInnerHTML={{ __html: `
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'AW-16624322208');
+          gtag('config', 'AW-17521374535');
         ` }} />
-        <Script type="text/javascript" async dangerouslySetInnerHTML={{ __html: `
-            var script = document.createElement('script');
-            script.async = true;
-            script.type = 'text/javascript';
-            var target = 'https://www.clickcease.com/monitor/stat.js';
-            script.src = target;
-            var elem = document.head;
-            elem.appendChild(script);
-          ` }} />
-           {/* Include gtag_report_conversion script */}
-        <Script dangerouslySetInnerHTML={{ __html: `
-          function gtag_report_conversion(url) {
-            var callback = function () {
-              if (typeof(url) != 'undefined') {
-                window.location = url;
-              }
-            };
-            gtag('event', 'conversion', {
-            'send_to': 'AW-16532858519/zpcMCL-KorgZEJfNvcs9',
-              'event_callback': callback
-            });
-            return false;
-          }
-        ` }} />
-
+        
 
 </head>
       <body className={inter.className}>
-        <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PGCK73CF" height="0" width="0" style={{ display: "none", visibility: "hidden" }} />
-          {/* <!--  ClickCease.com tracking--> */}
-          <a href="https://www.clickcease.com" rel="nofollow">
-            <img src="https://monitor.clickcease.com" alt="ClickCease" />
-          </a>
-          {/* <!--  ClickCease.com tracking--> */}
-        </noscript>
-      
+
       {children}
       </body>
       
